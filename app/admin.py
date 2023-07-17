cc4cfe69086aa41c907b6a8d0e5648d33ce23d55
mohammad foroutani <m4utani@gmail.com>
2023-07-17 18:05:19 +0330

    Add: admin and modified models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from app.models import Course, Term
+
+
 # Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'point')
+    readonly_fields = ('id',)
+
+
+admin.site.register(Course, CourseAdmin)
+
+class TermAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+    readonly_fields = ('id',)
+
+
+admin.site.register(Term, TermAdmin)
--- app/models.py
+++ app/models.py
@@ -1,31 +1,40 @@
 from django.db import models
 
-# Create your models here.
 
 class Profile(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    reshte = models.CharField(max_length=30)
+    birth_day = models.DateField(blank=True, null=True)
+    field = models.CharField(max_length=30)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
 
 class Student(models.Model):
-    number_std = models.IntegerField(null=False, blank=False)
-    profile = models.OneToOneField(Profile)
+    std_number = models.IntegerField(null=False, blank=False)
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
 
 
-class Techer(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-
-
-class Cours(models.Model):
-    name = models.CharField(max_length=30)
-    vahed = models.IntegerField()
+class Teacher(models.Model):
+    department = models.CharField(max_length=30)
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
 
 
 class Term(models.Model):
-    term = models.IntegerField(null=False, blank=False)
+    title = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=30)
+    point = models.IntegerField()
+    term = models.ForeignKey(Term, on_delete=models.CASCADE)
+
+
+class StudentCourse(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    mark = models.FloatField(blank=True, null=True)
